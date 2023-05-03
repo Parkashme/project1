68afd8013e49d36648b0fe6b90ee87a2522b302a
Parkashme <parkashbisht547@gmail.com>
2023-05-03 15:50:09 +0530

    First commit

---
+++ First.java
@@ -0,0 +1,6 @@
+class First{
+puclic static void main(String[] args){
+
+System.out.pritnln("WELCOME")
+}
+}
---
+++ second.java
@@ -0,0 +1,10 @@
+class EvenOdd{
+    public static void main (String[] args ){
+        int num=4;
+        if(num%2==0){
+            System.out.println("Number is even");
+        }else{
+            System.out.println("Number is odd");
+        }
+    }
+}
